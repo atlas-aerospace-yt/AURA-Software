3f0a4e1853cdb9b4bdecc121f761c1b678ec0a7b
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-08-01 01:10:48 +0100

    Testing implementation

--- core/Inc/main.h
+++ core/Inc/main.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "driver/gpio.h"
 #include "stdio.h"
 #include "BMP280.h"
 #include "ICM20948.h"
+#include "INA219.h"
--- core/Src/main.cpp
+++ core/Src/main.cpp
@@ -6,27 +6,15 @@ extern "C" void app_main(void)
     i2c::i2c_bus my_bus(GPIO_NUM_8, GPIO_NUM_9);
     bmp280::bmp280 my_bmp(&my_bus);
     icm20948::icm20948 my_icm(&my_bus);
+    ina219::ina219 my_ina(&my_bus);
 
-    float data;
+    gpio_reset_pin(GPIO_NUM_5);
+    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT);
 
     while (true)
     {
-        /*
-        my_bmp.update();
-
-        data = my_bmp.get_temperature();
-        printf("Recorded temperature: %fC\n", data);
-        data = my_bmp.get_pressure();
-        printf("Recorded pressure: %fkPa\n", data);
-        data = my_bmp.get_altitude();
-        printf("Recorded altiitude: %fm\n", data);
-        */
-        ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, PWR_MGMT_1, ON_MODE));
-        ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, PWR_MGMT_2, ON_MODE));
-  
-        int16_t icm_test;
-        icm_test = my_bus.read_bytes_i2c<int16_t,2>(my_icm._icm_handle, ICM_GYRO_XOUT);
-        printf("%d\n", icm_test);
-        vTaskDelay(pdMS_TO_TICKS(25));
+        printf("Voltage: %f\n", my_ina.get_voltage());
+        my_ina.get_current(); // TODO stop outputting 0 by calibrating
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }

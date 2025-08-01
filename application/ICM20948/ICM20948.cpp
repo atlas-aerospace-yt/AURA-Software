272f1e3cc964c732852dbc75e76d8e0a8508edd5
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-08-01 01:10:27 +0100

    Reading correct gyro and accel

--- application/ICM20948/ICM20948.cpp
+++ application/ICM20948/ICM20948.cpp
@@ -1,1 +1,124 @@
 #include "ICM20948.h"
+
+/*
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_PWR_MGMT_1, ON_MODE));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_PWR_MGMT_2, ON_MODE));
+
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_REG_BANK_SEL, BANK_SEL_2));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_GYRO_SMPLRT_DIV, GYRO_SMPLRT_DIV));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_ACCEL_SMPLRT_DIV_1, ACCEL_SMPLRT_DIV));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_ACCEL_SMPLRT_DIV_2, ACCEL_SMPLRT_DIV));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_GYRO_CONFIG_1, GYRO_CONFIG_1));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_GYRO_CONFIG_1, GYRO_CONFIG_2));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_ACCEL_CONFIG_1, ACCEL_CONFIG_1));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_ACCEL_CONFIG_2, ACCEL_CONFIG_2));
+ESP_ERROR_CHECK(my_bus.write_byte_i2c(my_icm._icm_handle, ICM_REG_BANK_SEL, BANK_SEL_0));
+
+int16_t icm_test;
+icm_test = my_bus.read_bytes_i2c<int16_t,2>(my_icm._icm_handle, ICM_ACC_XOUT);
+printf("%f\n", (static_cast<float>(icm_test)) / 2048.0f * 9.81f);
+vTaskDelay(pdMS_TO_TICKS(250));
+
+*/
+
+void icm20948::icm20948::_wake_device(void)
+{
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_PWR_MGMT_1,
+        ON_MODE
+    ));
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_PWR_MGMT_2,
+        ON_MODE
+    ));
+}
+
+void icm20948::icm20948::_select_register(uint8_t reg)
+{
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_REG_BANK_SEL,
+        reg
+    ));
+}
+
+void icm20948::icm20948::_configure_gyro(void)
+{
+    _select_register(BANK_SEL_2);
+
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_GYRO_SMPLRT_DIV,
+        GYRO_SMPLRT_DIV
+    ));
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_GYRO_CONFIG_1,
+        GYRO_CONFIG_1
+    ));
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_GYRO_CONFIG_2,
+        GYRO_CONFIG_2
+    ));
+
+    _select_register(BANK_SEL_0);
+}
+
+void icm20948::icm20948::_configure_accel(void)
+{
+    _select_register(BANK_SEL_2);
+
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_ACCEL_SMPLRT_DIV_1,
+        ACCEL_SMPLRT_DIV
+    ));
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_ACCEL_SMPLRT_DIV_2,
+        ACCEL_SMPLRT_DIV
+    ));
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_ACCEL_CONFIG_1,
+        ACCEL_CONFIG_1
+    ));
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_ACCEL_CONFIG_2,
+        ACCEL_CONFIG_2
+    ));
+
+    _select_register(BANK_SEL_0);
+}
+
+void icm20948::icm20948::_configure_mag(void)
+{
+    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
+        _icm_handle,
+        ICM_INT_PIN_CFG,
+        INT_PIN_CFG
+    ));
+}
+
+void icm20948::icm20948::update(void)
+{
+    uint64_t gyro, acc;
+    float gyro_x, gyro_y, gyro_z, acc_x, acc_y, acc_z;
+
+    acc = _master_bus->read_bytes_i2c<uint64_t,6>(_icm_handle, ICM_ACC_XOUT);
+    gyro = _master_bus->read_bytes_i2c<uint64_t,6>(_icm_handle, ICM_GYRO_XOUT);
+
+    acc_x = static_cast<float>(static_cast<int16_t>((acc >> 32) & AXIS_MASK)) / 2048.0f * 9.81f;
+    acc_y = static_cast<float>(static_cast<int16_t>((acc >> 16) & AXIS_MASK)) / 2048.0f * 9.81f;
+    acc_z = static_cast<float>(static_cast<int16_t>((acc) & AXIS_MASK)) / 2048.0f * 9.81f;
+
+    gyro_x = static_cast<float>(static_cast<int16_t>((gyro >> 32) & AXIS_MASK)) / 2048.0f * 125.0f;
+    gyro_y = static_cast<float>(static_cast<int16_t>((gyro >> 16) & AXIS_MASK)) / 2048.0f * 125.0f;
+    gyro_z = static_cast<float>(static_cast<int16_t>((gyro) & AXIS_MASK)) / 2048.0f * 125.0f;
+
+    printf("%f\n %f\n %f\n %f\n %f\n %f\n\n\n", acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z);
+}
--- application/ICM20948/ICM20948.h
+++ application/ICM20948/ICM20948.h
@@ -7,33 +7,70 @@
 #include "I2C.h"
 
 #define ICM_ADDR 0x68
+#define I2C_SPEED 100000
 
 #define CHIPID 0x00
-#define USER_CTRL 0x03
-#define LP_CONFIG 0x05
-#define PWR_MGMT_1 0x06
-#define PWR_MGMT_2 0x07
 
+/* Register bank selection*/
+#define ICM_REG_BANK_SEL 0x7F
+#define BANK_SEL_0 0x00
+#define BANK_SEL_1 0x10
+#define BANK_SEL_2 0x20
+#define BANK_SEL_3 0x30
+
+/* Setup register bank 0 addresses */
+#define ICM_USER_CTRL 0x03
+#define ICM_LP_CONFIG 0x05
+#define ICM_PWR_MGMT_1 0x06
+#define ICM_PWR_MGMT_2 0x07
+#define ICM_INT_PIN_CFG 0x0F
+
+/* Setup register bank 2 addresses */
+#define ICM_GYRO_CONFIG_1 0x01
+#define ICM_GYRO_CONFIG_2 0x02
+#define ICM_GYRO_SMPLRT_DIV 0x00
+
+#define ICM_ACCEL_CONFIG_1 0x14
+#define ICM_ACCEL_CONFIG_2 0x15
+#define ICM_ACCEL_SMPLRT_DIV_1 0x10
+#define ICM_ACCEL_SMPLRT_DIV_2 0x11
+
+/* Accel register addresses (BANK 0) */
 #define ICM_ACC_XOUT 0x2D
 #define ICM_ACC_YOUT 0x2F
 #define ICM_ACC_ZOUT 0x31
 
+/* GYRO register addresses (BANK 0) */
 #define ICM_GYRO_XOUT 0x33
 #define ICM_GYRO_YOUT 0x35
 #define ICM_GYRO_ZOUT 0x37
 
 #define ICM_TEMP_OUT 0x39
 
-#define I2C_SPEED 100000
+/* AK09916 Magnetometer definitions*/
+#define MAG_ADDR 0x0C
+#define MAG 1
 
+/* Initial settings values */
 #define ON_MODE 0x00
+#define LP_CONFIG 0x00
+#define INT_PIN_CFG 0x52
+
+#define GYRO_CONFIG_1 0x37
+#define GYRO_CONFIG_2 0x02
+#define GYRO_SMPLRT_DIV 0x00
+
+#define ACCEL_CONFIG_1 0x37
+#define ACCEL_CONFIG_2 0x02
+#define ACCEL_SMPLRT_DIV 0x00
+
+#define AXIS_MASK 0xffff
 
 namespace icm20948
 {
     class icm20948
     {
-    public:
-    //private:
+    private:
         i2c::i2c_bus* _master_bus;
 
         i2c_master_dev_handle_t _icm_handle;
@@ -41,7 +78,13 @@ namespace icm20948
 
         const uint8_t _icm_addr;
 
-    //public:
+        void _wake_device(void);
+        void _select_register(uint8_t reg);
+        void _configure_gyro(void);
+        void _configure_accel(void);
+        void _configure_mag(void);
+
+    public:
         icm20948(i2c::i2c_bus* master_bus, uint8_t addr=ICM_ADDR)
             : _master_bus(master_bus), _icm_addr(addr)
         {
@@ -53,6 +96,14 @@ namespace icm20948
                 &_icm_config,
                 &_icm_handle
             ));
+
+            _wake_device();
+            _configure_gyro();
+            _configure_accel();
+            _configure_mag();
         }
+
+        void update(void);
+
     };
 };

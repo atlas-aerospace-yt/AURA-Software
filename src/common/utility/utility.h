86139f1e20d475762ee03366269db3c35ddb5baa
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-08-05 17:58:42 +0100

    Started a utility library

---
+++ src/common/utility/utility.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define MAX_DELAY 100
+
+#define TOP ",1,"
+#define BOT ",2,"
+#define GRAPHING_BEGINNING "l058~"
+#define GRAPHING_ENDING "zC43_"
+#define GRAPH(name, data, type)         \
+    printf(GRAPHING_BEGINNING);         \
+    printf(name);                       \
+    printf(type);                       \
+    printf("%f", data);                 \
+    printf(GRAPHING_ENDING);            
+
+struct sensor_data {
+    float gyro_x, gyro_y, gyro_z, dt;
+};

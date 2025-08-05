c14dd3e4d67aa4b18074a9568186e9f39dbc0931
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-08-05 18:00:05 +0100

    Dual core example

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,92 +1,31 @@
 #include "main.h"
 
-void my_test_function(void* args)
-{
-    while (true)
-    {
-        printf("CORE 2 doing stuff!\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
 extern "C" void app_main(void)
 {
 
-    spi::create_spi_bus();
-
-    sdmmc_card_t sd_card;
-
-    sd_card::mount_sd_card(&sd_card);
-
-    std::ofstream MyFile("/sdcard/test.txt");
-    MyFile << "Hello World!";
-    MyFile.close();
-
-    sd_card::unmount_sd_card(&sd_card);
-
-    i2c::i2c_bus my_bus(GPIO_NUM_8, GPIO_NUM_9);
-    bmp280::bmp280 my_bmp(&my_bus);
-    icm20948::icm20948 my_icm(&my_bus);
-    //ina219::ina219 my_ina(&my_bus);
-
-    Quat rate;
-    Quat gyro = {0.0f, 0.0f, 0.0f, 0.0f};
-    Quat ori_quat = {1.0f, 0.0f, 0.0f, 0.0f};
-    Vect ori_euler = {0.0f, 0.0f, 0.0f};
-
-    float dt;
-    float x_offs = 0.0f, y_offs = 0.0f, z_offs = 0.0f;
-
-    int64_t delta_us, now;
-    int64_t last_time = esp_timer_get_time();
-
-    for (int i=0; i<1000; i++)
-    {
-        my_icm.update();
-        x_offs += my_icm.gyro_x;
-        y_offs += my_icm.gyro_y;
-        z_offs += my_icm.gyro_z;
-        vTaskDelay(pdMS_TO_TICKS(3));
-    }
-
-    x_offs /= 1000.0f;
-    y_offs /= 1000.0f;
-    z_offs /= 1000.0f;
-
-    xTaskCreatePinnedToCore(
-        my_test_function,
-        "Core2",
-        4096,
-        NULL,
-        tskIDLE_PRIORITY + 1,
-        NULL,
-        1
+    sensor_data example_data;
+    QueueHandle_t controlOutputQueue = xQueueCreate(
+        1,
+        sizeof(example_data)
     );
 
-    while (true)
-    {
-        now = esp_timer_get_time();
-        delta_us = now - last_time;
-        last_time = now;
+    xTaskCreatePinnedToCore(
+        core_0_task,
+        "Core_0",
+        10240,
+        (void *) controlOutputQueue,
+        MOST_PRIORITY,
+        NULL,
+        0
+    );
 
-        if (delta_us < 0) delta_us = 0;
-        dt = (float)delta_us / 1e6f;
-
-        my_icm.update();
-        gyro.i = my_icm.gyro_x - x_offs;
-        gyro.j = my_icm.gyro_y - y_offs;
-        gyro.k = my_icm.gyro_z - z_offs;
-
-
-        rate = ori_quat * 0.5f * gyro;
-        ori_quat = ori_quat + rate * dt;
-
-        ori_euler = ori_quat.To_Euler();
-        ori_euler = ori_euler.To_Degrees();
-
-        GRAPH("x", ori_euler.x, TOP);
-        GRAPH("y", ori_euler.y, TOP);
-        GRAPH("z", ori_euler.z, TOP);
-        printf("%.6f\n", dt);
-    }
+    xTaskCreatePinnedToCore(
+        core_1_task,
+        "Core_1",
+        10240,
+        (void *) controlOutputQueue,
+        LEAST_PRIORITY,
+        NULL,
+        0
+    );
 }
--- src/main/main.h
+++ src/main/main.h
@@ -2,35 +2,16 @@
 
 /* Standard */
 #include <fstream>
+#include <string>
 
 /* FreeRTOS */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-/* ESP IDF */
-#include "esp_timer.h"
+/* Core tasks*/
+#include "main_core_0.h"
+#include "main_core_1.h"
 
-/* Bus drivers*/
-#include "SPI.h"
-#include "I2C.h"
-
-/* Device drivers */
-#include "SD_Card.h"
-#include "BMP280.h"
-#include "ICM20948.h"
-#include "INA219.h"
-
-/* External */
-#include "Quat.h"
-
-#define TOP ",1,"
-#define BOT ",2,"
-#define GRAPHING_BEGINNING "l058~"
-#define GRAPHING_ENDING "zC43_"
-#define GRAPH(name, data, type)         \
-    printf(GRAPHING_BEGINNING);         \
-    printf(name);                       \
-    printf(type);                       \
-    printf("%f", data);                       \
-    printf(GRAPHING_ENDING);            \
+#define MOST_PRIORITY 20
+#define LEAST_PRIORITY tskIDLE_PRIORITY + 1

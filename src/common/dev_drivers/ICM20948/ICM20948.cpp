bfe198e3c2fca933f4648fb398a2fed9ed5b8cc6
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-08-14 23:54:41 +0100

    Added namespace and tidied up styling.

--- src/common/dev_drivers/ICM20948/ICM20948.cpp
+++ src/common/dev_drivers/ICM20948/ICM20948.cpp
@@ -1,145 +1,101 @@
 #include "ICM20948.h"
 
-void icm20948::icm20948::_wake_device(void)
-{
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_PWR_MGMT_1,
-        PWR_MGMT_1
-    ));
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_PWR_MGMT_2,
-        PWR_MGMT_2
-    ));
+namespace icm20948 {
+
+icm20948::icm20948(i2c::i2c_bus* master_bus) : _master_bus(master_bus) {
+  _icm_config.dev_addr_length = I2C_ADDR_BIT_LEN_7;
+  _icm_config.scl_speed_hz = I2C_SPEED;
+  _icm_config.device_address = ICM_ADDR;
+
+  ESP_ERROR_CHECK(_master_bus->add_slave_to_bus(&_icm_config, &_icm_handle));
+
+  _set_i2c_bypass();
+
+  _mag_config.dev_addr_length = I2C_ADDR_BIT_LEN_7;
+  _mag_config.scl_speed_hz = I2C_SPEED;
+  _mag_config.device_address = MAG_ADDR;
+
+  ESP_ERROR_CHECK(_master_bus->add_slave_to_bus(&_mag_config, &_mag_handle));
+
+  _wake_device();
+  _configure_gyro();
+  _configure_accel();
+  _configure_mag();
 }
 
-void icm20948::icm20948::_select_register(uint8_t reg)
-{
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_REG_BANK_SEL,
-        reg
-    ));
+void icm20948::_wake_device() {
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_PWR_MGMT_1, PWR_MGMT_1));
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_PWR_MGMT_2, PWR_MGMT_2));
 }
 
-void icm20948::icm20948::_configure_gyro(void)
-{
-    _select_register(BANK_SEL_2);
-
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_GYRO_SMPLRT_DIV,
-        GYRO_SMPLRT_DIV
-    ));
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_GYRO_CONFIG_1,
-        GYRO_CONFIG_1
-    ));
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_GYRO_CONFIG_2,
-        GYRO_CONFIG_2
-    ));
-
-    _select_register(BANK_SEL_0);
+void icm20948::_select_register(uint8_t reg) {
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_REG_BANK_SEL, reg));
 }
 
-void icm20948::icm20948::_configure_accel(void)
-{
-    _select_register(BANK_SEL_2);
+void icm20948::_configure_gyro() {
+  _select_register(BANK_SEL_2);
 
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_ACCEL_SMPLRT_DIV_1,
-        ACCEL_SMPLRT_DIV
-    ));
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_ACCEL_SMPLRT_DIV_2,
-        ACCEL_SMPLRT_DIV
-    ));
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_ACCEL_CONFIG_1,
-        ACCEL_CONFIG_1
-    ));
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_ACCEL_CONFIG_2,
-        ACCEL_CONFIG_2
-    ));
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_GYRO_SMPLRT_DIV, GYRO_SMPLRT_DIV));
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_GYRO_CONFIG_1, GYRO_CONFIG_1));
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_GYRO_CONFIG_2, GYRO_CONFIG_2));
 
-    _select_register(BANK_SEL_0);
+  _select_register(BANK_SEL_0);
 }
 
-void icm20948::icm20948::_set_i2c_bypass(void){
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _icm_handle,
-        ICM_INT_PIN_CFG,
-        INT_PIN_CFG
-    ));
+void icm20948::_configure_accel() {
+  _select_register(BANK_SEL_2);
+
+  ESP_ERROR_CHECK(
+      _master_bus->write_byte_i2c(_icm_handle, ICM_ACCEL_SMPLRT_DIV_1, ACCEL_SMPLRT_DIV));
+  ESP_ERROR_CHECK(
+      _master_bus->write_byte_i2c(_icm_handle, ICM_ACCEL_SMPLRT_DIV_2, ACCEL_SMPLRT_DIV));
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_ACCEL_CONFIG_1, ACCEL_CONFIG_1));
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_ACCEL_CONFIG_2, ACCEL_CONFIG_2));
+
+  _select_register(BANK_SEL_0);
 }
 
-void icm20948::icm20948::_configure_mag(void)
-{
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _mag_handle,
-        MAG_CNTRL_2,
-        MAG_SLEEP
-    ));
-
-    vTaskDelay(pdMS_TO_TICKS(10));
-
-    ESP_ERROR_CHECK(_master_bus->write_byte_i2c(
-        _mag_handle,
-        MAG_CNTRL_2,
-        MAG_CONT_MODE_4
-    ));
-
-    vTaskDelay(pdMS_TO_TICKS(10));
+void icm20948::_set_i2c_bypass() {
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_icm_handle, ICM_INT_PIN_CFG, INT_PIN_CFG));
 }
 
-void icm20948::icm20948::update(void)
-{
-    uint64_t gyro, acc;
+void icm20948::_configure_mag() {
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_mag_handle, MAG_CNTRL_2, MAG_SLEEP));
 
-    acc = _master_bus->read_bytes_i2c<uint64_t,6>(_icm_handle, ICM_ACC_XOUT);
-    gyro = _master_bus->read_bytes_i2c<uint64_t,6>(_icm_handle, ICM_GYRO_XOUT);
+  vTaskDelay(pdMS_TO_TICKS(10));
 
-    acc_x = static_cast<float>(static_cast<int16_t>(
-        (acc >> 32) & AXIS_MASK)) / CONVERT_ACCEL;
-    acc_y = static_cast<float>(static_cast<int16_t>(
-        (acc >> 16) & AXIS_MASK)) / CONVERT_ACCEL;
-    acc_z = static_cast<float>(static_cast<int16_t>(
-        (acc) & AXIS_MASK)) / CONVERT_ACCEL;
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_mag_handle, MAG_CNTRL_2, MAG_CONT_MODE_4));
 
-    gyro_x = static_cast<float>(static_cast<int16_t>(
-        (gyro >> 32) & AXIS_MASK)) / CONVERT_GYRO;
-    gyro_y = static_cast<float>(static_cast<int16_t>(
-        (gyro >> 16) & AXIS_MASK)) / CONVERT_GYRO;
-    gyro_z = static_cast<float>(static_cast<int16_t>(
-        (gyro) & AXIS_MASK)) / CONVERT_GYRO;
+  vTaskDelay(pdMS_TO_TICKS(10));
 }
 
-/**
- * NOTE: mag must be read in the order shown - ST1, data, ST2
- */
-void icm20948::icm20948::update_mag(void)
-{
-    uint64_t mag;
+// NOLINTBEGIN(misc-magic-numbers, readability-magic-numbers)
+void icm20948::update() {
+  const uint64_t acc = _master_bus->read_bytes_i2c<uint64_t, 6>(_icm_handle, ICM_ACC_XOUT);
+  const uint64_t gyro = _master_bus->read_bytes_i2c<uint64_t, 6>(_icm_handle, ICM_GYRO_XOUT);
 
-    _master_bus->read_bytes_i2c<uint8_t, 1>(_mag_handle, MAG_ST1);
+  _acc_x = static_cast<float>(static_cast<int16_t>((acc >> 32) & AXIS_MASK)) / CONVERT_ACCEL;
+  _acc_y = static_cast<float>(static_cast<int16_t>((acc >> 16) & AXIS_MASK)) / CONVERT_ACCEL;
+  _acc_z = static_cast<float>(static_cast<int16_t>((acc >> 0) & AXIS_MASK)) / CONVERT_ACCEL;
 
-    mag = _master_bus->read_bytes_i2c<uint64_t, 6>(_mag_handle, MAG_HXL, true);
-
-    _master_bus->read_bytes_i2c<uint8_t, 1>(_mag_handle, MAG_ST2);
-
-    mag_x = static_cast<float>(static_cast<int16_t>(
-        (mag >> 32) & AXIS_MASK)) * CONVERT_MAG;
-    mag_y = static_cast<float>(static_cast<int16_t>(
-        (mag >> 16) & AXIS_MASK)) * CONVERT_MAG;
-    mag_z = static_cast<float>(static_cast<int16_t>(
-        (mag) & AXIS_MASK)) * CONVERT_MAG - MAG_Z_OFFS;
+  _gyro_x = static_cast<float>(static_cast<int16_t>((gyro >> 32) & AXIS_MASK)) / CONVERT_GYRO;
+  _gyro_y = static_cast<float>(static_cast<int16_t>((gyro >> 16) & AXIS_MASK)) / CONVERT_GYRO;
+  _gyro_z = static_cast<float>(static_cast<int16_t>((gyro >> 0) & AXIS_MASK)) / CONVERT_GYRO;
 }
+
+//
+// NOTE: mag must be read in the order shown - ST1, data, ST2
+//
+void icm20948::update_mag() {
+  _master_bus->read_bytes_i2c<uint8_t, 1>(_mag_handle, MAG_ST1);
+
+  const uint64_t mag = _master_bus->read_bytes_i2c<uint64_t, 6>(_mag_handle, MAG_HXL, true);
+
+  _master_bus->read_bytes_i2c<uint8_t, 1>(_mag_handle, MAG_ST2);
+
+  _mag_x = static_cast<float>(static_cast<int16_t>((mag >> 32) & AXIS_MASK)) * CONVERT_MAG;
+  _mag_y = static_cast<float>(static_cast<int16_t>((mag >> 16) & AXIS_MASK)) * CONVERT_MAG;
+  _mag_z = static_cast<float>(static_cast<int16_t>(mag & AXIS_MASK)) * CONVERT_MAG - MAG_Z_OFFS;
+}
+// NOLINTEND(misc-magic-numbers, readability-magic-numbers)
+}  // namespace icm20948
--- src/common/dev_drivers/ICM20948/ICM20948.h
+++ src/common/dev_drivers/ICM20948/ICM20948.h
@@ -1,157 +1,196 @@
 /**
- * 
+ *
  * ICM20948 datasheet -
  * https://invensense.tdk.com/wp-content/uploads/2016/06/DS-000189-ICM-20948-v1.3.pdf
- * 
- * AK09916 datasheet - 
+ *
+ * AK09916 datasheet -
  * https://www.y-ic.es/datasheet/78/SMDSW.020-2OZ.pdf
- * 
+ *
  */
 #pragma once
 
-#include "math.h"
+#include <cmath>
+
 #include "I2C.h"
 
-#define ICM_ADDR 0x68
-#define I2C_SPEED 100000
+namespace icm20948 {
 
-#define CHIPID 0x00
+constexpr uint32_t I2C_SPEED = 100000;
+
+/* ICM20948 default I2C address on AURA PCB */
+constexpr uint8_t ICM_ADDR = 0x68;
+
+/* Expected chip id (whoami) */
+constexpr uint8_t CHIPID = 0x00;
 
 /* Register bank selection*/
-#define ICM_REG_BANK_SEL 0x7F
-#define BANK_SEL_0 0x00
-#define BANK_SEL_1 0x10
-#define BANK_SEL_2 0x20
-#define BANK_SEL_3 0x30
+constexpr uint8_t ICM_REG_BANK_SEL = 0x7F;
+constexpr uint8_t BANK_SEL_0 = 0x00;
+constexpr uint8_t BANK_SEL_1 = 0x10;
+constexpr uint8_t BANK_SEL_2 = 0x20;
+constexpr uint8_t BANK_SEL_3 = 0x30;
 
 /* Setup register bank 0 addresses */
-#define ICM_USER_CTRL 0x03
-#define ICM_LP_CONFIG 0x05
-#define ICM_PWR_MGMT_1 0x06
-#define ICM_PWR_MGMT_2 0x07
-#define ICM_INT_PIN_CFG 0x0F
+constexpr uint8_t ICM_USER_CTRL = 0x03;
+constexpr uint8_t ICM_LP_CONFIG = 0x05;
+constexpr uint8_t ICM_PWR_MGMT_1 = 0x06;
+constexpr uint8_t ICM_PWR_MGMT_2 = 0x07;
+constexpr uint8_t ICM_INT_PIN_CFG = 0x0F;
 
 /* Setup register bank 2 addresses */
-#define ICM_GYRO_CONFIG_1 0x01
-#define ICM_GYRO_CONFIG_2 0x02
-#define ICM_GYRO_SMPLRT_DIV 0x00
+constexpr uint8_t ICM_GYRO_CONFIG_1 = 0x01;
+constexpr uint8_t ICM_GYRO_CONFIG_2 = 0x02;
+constexpr uint8_t ICM_GYRO_SMPLRT_DIV = 0x00;
 
-#define ICM_ACCEL_CONFIG_1 0x14
-#define ICM_ACCEL_CONFIG_2 0x15
-#define ICM_ACCEL_SMPLRT_DIV_1 0x10
-#define ICM_ACCEL_SMPLRT_DIV_2 0x11
+constexpr uint8_t ICM_ACCEL_CONFIG_1 = 0x14;
+constexpr uint8_t ICM_ACCEL_CONFIG_2 = 0x15;
+constexpr uint8_t ICM_ACCEL_SMPLRT_DIV_1 = 0x10;
+constexpr uint8_t ICM_ACCEL_SMPLRT_DIV_2 = 0x11;
 
 /* Accel register addresses (BANK 0) */
-#define ICM_ACC_XOUT 0x2D
-#define ICM_ACC_YOUT 0x2F
-#define ICM_ACC_ZOUT 0x31
+constexpr uint8_t ICM_ACC_XOUT = 0x2D;
+constexpr uint8_t ICM_ACC_YOUT = 0x2F;
+constexpr uint8_t ICM_ACC_ZOUT = 0x31;
 
 /* GYRO register addresses (BANK 0) */
-#define ICM_GYRO_XOUT 0x33
-#define ICM_GYRO_YOUT 0x35
-#define ICM_GYRO_ZOUT 0x37
+constexpr uint8_t ICM_GYRO_XOUT = 0x33;
+constexpr uint8_t ICM_GYRO_YOUT = 0x35;
+constexpr uint8_t ICM_GYRO_ZOUT = 0x37;
 
-#define ICM_TEMP_OUT 0x39
+constexpr uint8_t ICM_TEMP_OUT = 0x39;
 
 /* AK09916 Magnetometer definitions*/
-#define MAG_ADDR 0x0C
+constexpr uint8_t MAG_ADDR = 0x0C;
 
-#define MAG_ST1 0x10
-#define MAG_ST2 0x18
+constexpr uint8_t MAG_ST1 = 0x10;
+constexpr uint8_t MAG_ST2 = 0x18;
 
-#define MAG_CNTRL_2 0x31
-#define MAG_CNTRL_3 0x32
+constexpr uint8_t MAG_CNTRL_2 = 0x31;
+constexpr uint8_t MAG_CNTRL_3 = 0x32;
 
-#define MAG_HXL 0x11
-#define MAG_HXH 0x12
-#define MAG_HYL 0x13
-#define MAG_HTH 0x14
-#define MAG_HZL 0x15
-#define MAG_HZH 0x16
+constexpr uint8_t MAG_HXL = 0x11;
+constexpr uint8_t MAG_HXH = 0x12;
+constexpr uint8_t MAG_HYL = 0x13;
+constexpr uint8_t MAG_HTH = 0x14;
+constexpr uint8_t MAG_HZL = 0x15;
+constexpr uint8_t MAG_HZH = 0x16;
 
 /* Initial settings values */
-#define PWR_MGMT_1 0x01
-#define PWR_MGMT_2 0x00
-#define LP_CONFIG 0x00
-#define INT_PIN_CFG 0x52
+constexpr uint8_t PWR_MGMT_1 = 0x01;
+constexpr uint8_t PWR_MGMT_2 = 0x00;
+constexpr uint8_t LP_CONFIG = 0x00;
+constexpr uint8_t INT_PIN_CFG = 0x52;
 
-#define GYRO_CONFIG_1 0x37
-#define GYRO_CONFIG_2 0x02
-#define GYRO_SMPLRT_DIV 0x00
+constexpr uint8_t GYRO_CONFIG_1 = 0x37;
+constexpr uint8_t GYRO_CONFIG_2 = 0x02;
+constexpr uint8_t GYRO_SMPLRT_DIV = 0x00;
 
-#define ACCEL_CONFIG_1 0x37
-#define ACCEL_CONFIG_2 0x02
-#define ACCEL_SMPLRT_DIV 0x00
+constexpr uint8_t ACCEL_CONFIG_1 = 0x37;
+constexpr uint8_t ACCEL_CONFIG_2 = 0x02;
+constexpr uint8_t ACCEL_SMPLRT_DIV = 0x00;
 
-#define MAG_SLEEP 0x0
-#define MAG_CONT_MODE_4 0x08
+constexpr uint8_t MAG_SLEEP = 0x00;
+constexpr uint8_t MAG_CONT_MODE_4 = 0x08;
 
-#define AXIS_MASK 0xffff
+constexpr uint16_t AXIS_MASK = 0xffff;
 
 /* Conversion definitions */
-#define CONVERT_ACCEL 208.767f
-#define CONVERT_GYRO 938.74f
-#define CONVERT_MAG 0.15f
+constexpr float CONVERT_ACCEL = 208.767F;
+constexpr float CONVERT_GYRO = 938.74F;
+constexpr float CONVERT_MAG = 0.15F;
 
-#define MAG_Z_OFFS 45.0f
+constexpr float MAG_Z_OFFS = 45.0F;
 
-namespace icm20948
-{
-    class icm20948
-    {
-    private:
-        i2c::i2c_bus* _master_bus;
+//
+// A wrapper class to handle communication with an ICM20948 device connected via the I2c protocol
+//
+class icm20948 {
+ private:
+  i2c::i2c_bus* _master_bus;
 
-        // ICM20948 I2C handle
-        i2c_master_dev_handle_t _icm_handle;
-        i2c_device_config_t _icm_config = {};
+  // ICM20948 I2C handle
+  i2c_master_dev_handle_t _icm_handle;
+  i2c_device_config_t _icm_config = {};
 
-        // Internal AK09916 I2C handle
-        i2c_master_dev_handle_t _mag_handle;
-        i2c_device_config_t _mag_config = {};
+  // Internal AK09916 I2C handle
+  i2c_master_dev_handle_t _mag_handle;
+  i2c_device_config_t _mag_config = {};
 
-        void _wake_device(void);
-        void _select_register(uint8_t reg);
-        void _configure_gyro(void);
-        void _configure_accel(void);
-        void _set_i2c_bypass(void);
-        void _configure_mag(void);
+  // IMU data floats
+  float _gyro_x{};
+  float _gyro_y{};
+  float _gyro_z{};
+  float _acc_x{};
+  float _acc_y{};
+  float _acc_z{};
+  float _mag_x{};
+  float _mag_y{};
+  float _mag_z{};
 
-    public:
-        float gyro_x, gyro_y, gyro_z, acc_x, acc_y, acc_z;
-        float mag_x, mag_y, mag_z;
+  //
+  // Write to PWR_MGMT_1 and PWR_MGMT_2 to turn on accelerometer and gyroscope
+  //
+  void _wake_device();
 
-        icm20948(i2c::i2c_bus* master_bus)
-            : _master_bus(master_bus)
-        {
-            _icm_config.dev_addr_length = I2C_ADDR_BIT_LEN_7;
-            _icm_config.scl_speed_hz = I2C_SPEED;
-            _icm_config.device_address = ICM_ADDR;
+  //
+  // Set the gyroscope configuration to +-2000deg/s and configure the lowpass filter to a cuttoff
+  // frequency of 5Hz
+  //
+  void _configure_gyro();
 
-            ESP_ERROR_CHECK(_master_bus->add_slave_to_bus(
-                &_icm_config,
-                &_icm_handle
-            ));
+  //
+  // Set the accelerometer configuration to +-16g and configure the lowpass filter to a cuttoff
+  // frequency of 5Hz
+  //
+  void _configure_accel();
 
-            _set_i2c_bypass();
+  //
+  // The AK0991 is on a seperate I2C bus controlled by the ICM20948 so this makes the AK09916 show
+  // up on the same bus the ICM20948 is on
+  //
+  void _set_i2c_bypass();
 
-            _mag_config.dev_addr_length = I2C_ADDR_BIT_LEN_7;
-            _mag_config.scl_speed_hz = I2C_SPEED;
-            _mag_config.device_address = MAG_ADDR;
+  //
+  // Configure the magnetometer to wake the device up and set it to read data continuously
+  //
+  void _configure_mag();
 
-            ESP_ERROR_CHECK(_master_bus->add_slave_to_bus(
-                &_mag_config,
-                &_mag_handle
-            ));
+  //
+  // A convinience function which selects which register bank you are reading/writing to
+  //
+  // NOTE: you must select bank 0 with BANK_SEL_0 after changing as the ICM20948 does not
+  //       automatically deselect the chosen bank
+  //
+  void _select_register(uint8_t reg);
 
-            _wake_device();
-            _configure_gyro();
-            _configure_accel();
-            _configure_mag();
-        }
+ public:
+  explicit icm20948(i2c::i2c_bus* master_bus);
 
-        void update(void);
-        void update_mag(void);
+  [[nodiscard]] auto gyro_x() const -> float { return _gyro_x; }
+  [[nodiscard]] auto gyro_y() const -> float { return _gyro_y; }
+  [[nodiscard]] auto gyro_z() const -> float { return _gyro_z; }
 
-    };
+  [[nodiscard]] auto acc_x() const -> float { return _acc_x; }
+  [[nodiscard]] auto acc_y() const -> float { return _acc_y; }
+  [[nodiscard]] auto acc_z() const -> float { return _acc_z; }
+
+  [[nodiscard]] auto mag_x() const -> float { return _mag_x; }
+  [[nodiscard]] auto mag_y() const -> float { return _mag_y; }
+  [[nodiscard]] auto mag_z() const -> float { return _mag_z; }
+
+  //
+  // Read the accelerometer readings in one go and then the gyro readings in one go and convert to
+  // a float
+  //
+  auto update() -> void;
+
+  //
+  // Read all magnetometer readings in one go following the specific read order:
+  // 1 - Read ST1 to check if new data is ready
+  // 2 - Read all of the data in one go (HXH to HZL)
+  // 3 - Read ST2 to check if overflow occured
+  //
+  auto update_mag() -> void;
 };
+
+}  // namespace icm20948

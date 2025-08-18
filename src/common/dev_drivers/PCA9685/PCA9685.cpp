4bf3fbb44855eadabb8035576757084f29e3177d
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-08-18 17:21:26 +0100

    Created pca_esc class to make dealing with ESCs easier.

--- src/common/dev_drivers/PCA9685/PCA9685.cpp
+++ src/common/dev_drivers/PCA9685/PCA9685.cpp
@@ -1,16 +1,17 @@
 #include "PCA9685.h"
 
 namespace pca9685 {
-
-pca9685::pca9685(i2c::i2c_bus* master_bus, uint8_t addr)
-    : _master_bus(master_bus), _pca_addr(addr) {
+//
+// Method definitions for the pca9685 class
+//
+pca9685::pca9685(i2c::i2c_bus* master_bus, uint16_t freq, uint8_t addr)
+    : _master_bus(master_bus), _freq(freq), _pca_addr(addr) {
   // Setup the I2C device
   _pca_config.dev_addr_length = I2C_ADDR_BIT_LEN_7;
   _pca_config.scl_speed_hz = I2C_SPEED;
   _pca_config.device_address = _pca_addr;
 
   ESP_ERROR_CHECK(_master_bus->add_slave_to_bus(&_pca_config, &_pca_handle));
-
   // Hobby servos use 50Hz
   _set_freq();
   // Set all channels to be fully off
@@ -18,10 +19,12 @@ pca9685::pca9685(i2c::i2c_bus* master_bus, uint8_t addr)
 }
 
 auto pca9685::_set_freq() -> void {
+  // Calculate the PRE_SCALE value according to datasheet
+  const uint8_t pre_scale = static_cast<uint8_t>(PCA_INT_CLK_FREQ / (4096 * _freq) - 1);
   // Put the device in sleep mode
   ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_pca_handle, PCA_MODE1, SLEEP_MODE));
   // Set the pre scale value
-  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_pca_handle, PCA_PRE_SCALE, PRE_SCALE_50_HZ));
+  ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_pca_handle, PCA_PRE_SCALE, pre_scale));
   // Wake the device up
   ESP_ERROR_CHECK(_master_bus->write_byte_i2c(_pca_handle, PCA_MODE1, WAKE_MODE));
   // Set the output mode to not be open drain
@@ -48,6 +51,20 @@ auto pca9685::_set_freq() -> void {
                                              static_cast<uint8_t>((pulse_off >> 8) & 0x1F)};
   // Return the esp_err_t
   return _master_bus->write_bytes_i2c<4>(_pca_handle, ALL_LED_ON_L, pulse_reg_val);
+}
+
+//
+// Method definitions for the pca_esc class
+//
+pca_esc::pca_esc(pca9685* pca_handle, uint8_t channel)
+    : _pca_handle(pca_handle), _channel(channel) {
+  set_throttle(0);
+}
+
+[[nodiscard]] auto pca_esc::set_throttle(uint8_t percent) const -> esp_err_t {
+  const float percent_clamped = static_cast<float>(std::clamp(percent, ESC_OFF, ESC_ON)) / 100.0F;
+  const uint16_t pulse = static_cast<uint16_t>(percent_clamped * (_max - _min) + _min);
+  return _pca_handle->set_pwm(_channel, pulse);
 }
 
 }  // namespace pca9685
--- src/common/dev_drivers/PCA9685/PCA9685.h
+++ src/common/dev_drivers/PCA9685/PCA9685.h
@@ -22,6 +22,12 @@ constexpr uint8_t REG_ADDR_SEP_CNT = 0x04;
 
 constexpr uint32_t I2C_SPEED = 100000;
 
+constexpr uint8_t ESC_FREQ = 50;
+constexpr uint8_t ESC_OFF = 0;
+constexpr uint8_t ESC_ON = 100;
+
+constexpr uint32_t PCA_INT_CLK_FREQ = 25000000;
+
 constexpr uint8_t PCA_ADDR = 0x40;
 
 constexpr uint8_t PCA_MODE1 = 0x00;
@@ -48,8 +54,6 @@ constexpr uint8_t ALL_LED_ON_L = 0xFA;
 constexpr uint8_t PCA_PRE_SCALE = 0xFE;
 
 // The value for the PRE_SCALE register to set the frequency to 50Hz
-constexpr uint8_t PRE_SCALE_50_HZ = 0x79;
-
 constexpr uint8_t SLEEP_MODE = 0x10;
 constexpr uint8_t WAKE_MODE = 0x20;
 constexpr uint8_t RESTART = 0x80;
@@ -65,15 +69,17 @@ class pca9685 {
   i2c_master_dev_handle_t _pca_handle;
   i2c_device_config_t _pca_config{};
 
+  const uint16_t _freq;
   const uint8_t _pca_addr;
 
   //
-  // The PRE_SCALE register needs to be set to 50Hz based off of the 25MHz internal oscillator
+  // The PRE_SCALE register needs to be set to a frequency based off of the 25MHz internal
+  // oscillator
   //
   auto _set_freq() -> void;
 
  public:
-  explicit pca9685(i2c::i2c_bus* master_bus, uint8_t addr = PCA_ADDR);
+  explicit pca9685(i2c::i2c_bus* master_bus, uint16_t freq = ESC_FREQ, uint8_t addr = PCA_ADDR);
 
   //
   // Set the pulse width of a specific channel on the PCA9685
@@ -93,6 +99,44 @@ class pca9685 {
   // @returns esp_err_t the ESP status after writign to the PCA9685
   //
   [[nodiscard]] auto set_pwm_all(uint16_t pulse) const -> esp_err_t;
+};
+
+//
+// A wrapper class to make using ESCs easier with the PCA9685
+//
+class pca_esc {
+ private:
+  pca9685* _pca_handle;
+
+  bool _armed = false;
+
+  const uint8_t _channel;
+  const uint16_t _min = 205;
+  const uint16_t _max = 410;
+
+ public:
+  explicit pca_esc(pca9685* pca_handle, uint8_t channel);
+
+  //
+  // Set the throttle percentage of the motor
+  //
+  // @param percent 0-100, the percent of full throttle to set the motor to
+  //
+  [[nodiscard]] auto set_throttle(uint8_t percent) const -> esp_err_t;
+
+  //
+  // @returns bool the current state of armed
+  //
+  [[nodiscard]] auto armed() -> bool { return _armed; }
+
+  //
+  // Set the ESC to be armed or not armed
+  //
+  // <<< WARNING >>> When armed, motors may spin - remove propellers
+  //
+  // @param new_armed the new state of the armed variable
+  //
+  auto set_armed(bool new_armed) -> void { _armed = new_armed; }
 };
 
 }  // namespace pca9685

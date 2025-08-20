6cc4ab3c08e94c1256ee9652e9863066911488cd
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-08-20 01:07:31 +0100

    New iBus library for the FS-A8S FlySky receiver.

---
+++ src/common/dev_drivers/FSA8S/FSA8S.cpp
@@ -0,0 +1,49 @@
+#include "FSA8S.h"
+
+namespace fsa8s {
+
+receiver::receiver(gpio_num_t rx_pin, uart_port_t uart_port) : _uart_port(uart_port) {
+  uart_config_t uart_config = {};
+  uart_config.baud_rate = BAUD_RATE;
+  uart_config.data_bits = UART_DATA_8_BITS;
+  uart_config.parity = UART_PARITY_DISABLE;
+  uart_config.stop_bits = UART_STOP_BITS_1;
+  uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+
+  ESP_ERROR_CHECK(uart_param_config(_uart_port, &uart_config));
+  ESP_ERROR_CHECK(uart_driver_install(_uart_port, UART_BUF_SIZE, 0, 0, NULL, 0));
+  ESP_ERROR_CHECK(uart_set_pin(_uart_port, -1, rx_pin, -1, -1));
+}
+
+auto receiver::_recv_uart() -> void {
+  _data_len = static_cast<int16_t>(uart_read_bytes(_uart_port, &_data, sizeof(_data), 0));
+}
+
+auto receiver::_get_frame_start() const -> int16_t {
+  const uint8_t last_frame_start_pos = _data_len - 33;
+  for (int16_t i = last_frame_start_pos; i >= 0; i--) {
+    if (_data[i] == IBUS_START_1 && _data[i + 1] == IBUS_START_2) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+auto receiver::_parse_ch_vals(uint8_t frame_start) -> void {
+  _ch1 = _data[frame_start + 2] | _data[frame_start + 3] << 8;
+  _ch2 = _data[frame_start + 4] | _data[frame_start + 5] << 8;
+  _ch3 = _data[frame_start + 6] | _data[frame_start + 7] << 8;
+  _ch4 = _data[frame_start + 8] | _data[frame_start + 9] << 8;
+  _ch5 = _data[frame_start + 10] | _data[frame_start + 11] << 8;
+  _ch6 = _data[frame_start + 12] | _data[frame_start + 13] << 8;
+}
+
+auto receiver::update() -> void {
+  _recv_uart();
+  int16_t indx = _get_frame_start();
+  if (indx != -1) {
+    _parse_ch_vals(static_cast<uint8_t>(indx));
+  }
+}
+
+}  // namespace fsa8s
---
+++ src/common/dev_drivers/FSA8S/FSA8S.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+namespace fsa8s {
+
+constexpr uint32_t BAUD_RATE = 115200;
+
+constexpr uint8_t IBUS_START_1 = 0x20;
+constexpr uint8_t IBUS_START_2 = 0x40;
+
+constexpr size_t UART_BUF_SIZE = 256;
+
+//
+// A receiver object to deal with handling the FlySky iBus protocol
+//
+class receiver {
+ private:
+  uart_port_t _uart_port;
+
+  int _data_len{};
+  uint8_t _data[UART_BUF_SIZE]{};
+
+  uint16_t _ch1{};
+  uint16_t _ch2{};
+  uint16_t _ch3{};
+  uint16_t _ch4{};
+  uint16_t _ch5{};
+  uint16_t _ch6{};
+
+  //
+  // Put the data from UART into the buffer to be processed.
+  //
+  auto _recv_uart() -> void;
+
+  //
+  // Get the index of the 0x20 from the start bytes of the iBus protocol
+  //
+  // @returns int16_t -1 to BUFFER_SIZE where -1 is only if the start is not found
+  //
+  auto _get_frame_start() const -> int16_t;
+
+  //
+  // Convert the data in the buffer into useable values (1000-2000) which are saved in the _ch*
+  // variables
+  //
+  // @params frame_start the index of the start of the full frame
+  //
+  auto _parse_ch_vals(uint8_t frame_start) -> void;
+
+ public:
+  explicit receiver(gpio_num_t rx_pin, uart_port_t uart_port = UART_NUM_1);
+
+  auto ch1() const -> uint16_t { return _ch1; }
+  auto ch2() const -> uint16_t { return _ch2; }
+  auto ch3() const -> uint16_t { return _ch3; }
+  auto ch4() const -> uint16_t { return _ch4; }
+  auto ch5() const -> uint16_t { return _ch5; }
+  auto ch6() const -> uint16_t { return _ch6; }
+
+  //
+  // Read the UART data from the FS-A8S and parse the data to update the channel values
+  //
+  auto update() -> void;
+};
+
+}  // namespace fsa8s

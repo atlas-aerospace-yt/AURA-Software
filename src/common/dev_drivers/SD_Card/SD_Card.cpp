3c865c85741a77f96c98c985010e50cdd351dcc1
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-09-01 22:33:08 +0100

    Turned SD card into a class.

--- src/common/dev_drivers/SD_Card/SD_Card.cpp
+++ src/common/dev_drivers/SD_Card/SD_Card.cpp
@@ -1,28 +1,25 @@
 #include "SD_Card.h"
 
-void sd_card::mount_sd_card(sdmmc_card_t *sd_card)
-{
-    esp_vfs_fat_mount_config_t file_config = {};
-    file_config.format_if_mount_failed = false;
-    file_config.max_files = MAX_FILE_COUNT;
+namespace sd_card {
 
-    sdmmc_host_t sd_host = SDSPI_HOST_DEFAULT();
-    sd_host.slot = SPI_BUS;
+sd_card::sd_card(gpio_num_t cs_pin, spi_host_device_t spi_bus) {
+  esp_vfs_fat_mount_config_t mount_config = {};
+  mount_config.format_if_mount_failed = false;
+  mount_config.max_files = MAX_FILE_COUNT;
 
-    sdspi_device_config_t sd_config = SDSPI_DEVICE_CONFIG_DEFAULT();
-    sd_config.gpio_cs = GPIO_NUM_13;
-    sd_config.host_id = SPI_BUS;
+  sdmmc_host_t sd_host = SDSPI_HOST_DEFAULT();
+  sd_host.slot = spi_bus;
 
-    ESP_ERROR_CHECK(esp_vfs_fat_sdspi_mount(
-        SD_DIR,
-        &sd_host,
-        &sd_config,
-        &file_config,
-        &sd_card
-    ));
+  sdspi_device_config_t sd_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+  sd_config.gpio_cs = cs_pin;
+  sd_config.host_id = spi_bus;
+
+  ESP_ERROR_CHECK(esp_vfs_fat_sdspi_mount(SD_DIR, &sd_host, &sd_config,
+                                          &mount_config, &_sd_card));
 }
 
-void sd_card::unmount_sd_card(sdmmc_card_t *sd_card)
-{
-    esp_vfs_fat_sdcard_unmount(SD_DIR, sd_card);
+void sd_card::unmount_sd_card() {
+  esp_vfs_fat_sdcard_unmount(SD_DIR, _sd_card);
 }
+
+}  // namespace sd_card
--- src/common/dev_drivers/SD_Card/SD_Card.h
+++ src/common/dev_drivers/SD_Card/SD_Card.h
@@ -1,17 +1,37 @@
 #pragma once
 
+#include "SPI.h"
 #include "driver/sdmmc_types.h"
 #include "driver/sdspi_host.h"
-
 #include "esp_vfs_fat.h"
 
-#include "SPI.h"
+namespace sd_card {
 
-#define SD_DIR "/sd_card"
-#define MAX_FILE_COUNT 5
+constexpr const char *SD_DIR = "/sd_card";
+constexpr uint8_t MAX_FILE_COUNT = 5;
 
-namespace sd_card
-{
-    void mount_sd_card(sdmmc_card_t*);
-    void unmount_sd_card(sdmmc_card_t*);
-}
+//
+// A very small class to keep the SD card code neat
+//
+class sd_card {
+ private:
+  sdmmc_card_t *_sd_card;
+
+ public:
+  //
+  // Add the SD card to the SPI bus
+  //
+  explicit sd_card(gpio_num_t cs_pin, spi_host_device_t spi_bus);
+
+  //
+  // Mount the fatfs formatted SD card
+  //
+  auto mount_sd_card(spi_host_device_t spi_bus) -> void;
+
+  //
+  // Unmount the SD card
+  //
+  auto unmount_sd_card() -> void;
+};
+
+}  // namespace sd_card
